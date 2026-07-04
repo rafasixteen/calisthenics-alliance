3ab4c84baa1986856db828da68d1eb2416fd9553
rafasixteen-laptop <rafaelrego16@gmail.com>
2026-07-04 23:42:57 +0100

    removed base path. changed npm run start cmd to serve the build folder

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
 
-console.log("isGitHubPages", isGitHubPages);
-
 const nextConfig: NextConfig = {
 	output: "export",
 	images: {
 		unoptimized: true,
 	},
-	basePath: isGitHubPages ? "/calisthenics-alliance" : "",
 	assetPrefix: isGitHubPages ? "/calisthenics-alliance/" : undefined,
 };
 
