2016ecedf87687611a797bf9cbdd8b3f13096130
DESKTOP-ASDSBPG\rafasixteen <rafaelrego16@gmail.com>
2026-07-08 19:45:32 +0100

    fixed url import

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
-import { getBasePath } from "@/lib/cn";
+import { getBasePath } from "@/lib/url";
 
 const withNextIntl = createNextIntlPlugin();
 
