bc53d09add53edd2d1a38b03f471cd25095bd183
DESKTOP-ASDSBPG\rafasixteen <rafaelrego16@gmail.com>
2026-07-09 04:04:34 +0100

    added events link in navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
 
 	return (
 		<header className="fixed inset-x-0 top-0 z-50 border-b bg-background/70 backdrop-blur supports-backdrop-filter:bg-background/60">
-			<div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4 md:grid md:grid-cols-3">
+			<div className="mx-auto flex h-16 max-w-4xl items-center justify-between px-4 md:grid md:grid-cols-3">
 				{/* Left: Logo */}
 				<div className="flex justify-start">
 					<Link href="/" className="font-semibold tracking-tight">
@@ -30,6 +30,10 @@ export function Navbar() {
 
 					<Link href="/rankings" className="transition hover:text-foreground">
 						{translations("rankings")}
+					</Link>
+
+					<Link href="/events" className="transition hover:text-foreground">
+						{translations("events")}
 					</Link>
 				</nav>
 			</div>
