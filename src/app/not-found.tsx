f096ea5038d21ff4920dab66561a0eb50c0b7ff7
DESKTOP-ASDSBPG\rafasixteen <rafaelrego16@gmail.com>
2026-07-11 03:35:16 +0100

    npm run format

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,11 @@
-"use client";
-
-import Error from "next/error";
-
-// Render the default Next.js 404 page when a route
-// is requested that doesn't match the middleware and
-// therefore doesn't have a locale associated with it.
 export default function NotFound() {
 	return (
 		<html lang="en">
 			<body>
-				<Error statusCode={404} />
+				<h1>404</h1>
+				<p> The page you are looking for does not exist.</p>
+				<p> Please check the URL and try again.</p>
+				<p> If you think this is a mistake, please contact support.</p>
 			</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { routing } from "@/i18n/routing";
 
 export default function Page() {
